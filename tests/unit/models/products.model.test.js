4ceaf64ca6424e4fabbccc77177a19719074da3e
Vinicius Luiz de Souza Reis <viniciusluizdesouzareis@pop-os.localdomain>
2022-11-11 16:43:20 -0300

    Requisito 2 fixed

--- tests/unit/models/products.model.test.js
+++ tests/unit/models/products.model.test.js
@@ -1,8 +1,7 @@
 const { expect } = require('chai');
-const chai = require('chai');
-const chaiHttp = require('chai-http');
+
 const sinon = require('sinon');
-const app = require('../../../src/app');
+const productsModel = require('../../../src/models/products.model');
 const allProducts = require('./mocks/allProducts.model.mock');
 
 const connection = require('../../../src/database/connection');
@@ -11,21 +10,15 @@ describe('Testando os endpoints de products', function () {
   afterEach(sinon.restore);
   it('Testando a listagem de todas os produtos', async function () {
     sinon.stub(connection, 'execute').resolves([allProducts]);
-    const response = await chai
-      .request(app)
-      .get('/products');
+    const result = await productsModel.findAll();
 
-    expect(response.status).to.equal(200);
-    expect(response.body).to.deep.equal(allProducts);
+    expect(result).to.deep.equal(allProducts);
   });
 
   it('Testando a listagem da produtos com id 1', async function () {
     sinon.stub(connection, 'execute').resolves([[allProducts[0]]]);
-    const response = await chai
-      .request(app)
-      .get('/products/1');
+    const result = await productsModel.findById();
 
-    expect(response.status).to.equal(200);
-    expect(response.body).to.deep.equal(allProducts[0]);
+    expect(result).to.deep.equal(allProducts[0]);
   });
 });
