3d95faf45ddc0825b01c0a54b7e72a73ad9af201
Vinicius Luiz de Souza Reis <viniciusluizdesouzareis@pop-os.localdomain>
2022-11-14 16:29:00 -0300

    Verifica se testes passam no github

--- tests/unit/models/mocks/allSales.model.mock.js
+++ tests/unit/models/mocks/allSales.model.mock.js
@@ -18,3 +18,21 @@ const allSalesDB = [
     "quantity": 15
   }
 ]
+
+const idSalesDb = [
+  {
+    "date": "2022-11-14T18:54:41.000Z",
+    "productId": 1,
+    "quantity": 5
+  },
+  {
+    "date": "2022-11-14T18:54:41.000Z",
+    "productId": 2,
+    "quantity": 10
+  }
+];
+
+module.exports = {
+  allSalesDB,
+  idSalesDb
+}
--- tests/unit/models/sales.model.test.js
+++ tests/unit/models/sales.model.test.js
@@ -6,6 +6,7 @@ const sinon = require('sinon');
 
 const salesModel = require('../../../src/models/sales.model');
 const { allProducts } = require('./mocks/allProducts.model.mock');
+const { idSalesDb, allSalesDB } = require('./mocks/allSales.model.mock');
 
 const app = require('../../../src/app');
 const connection = require('../../../src/database/connection');
@@ -13,136 +14,26 @@ const connection = require('../../../src/database/connection');
 use(chaiHttp);
 
 describe('Testando os endpoints de sales', function () {
+  it('Testando a listagem de sales ', async function () {
+    sinon.stub(connection, 'execute').resolves([allSalesDB]);
+    const response = await chai
+      .request(app)
+      .get('/sales')
+      
+
+    expect(response.status).to.equal(200);
+    expect(response.body).to.deep.equal(allSalesDB);
+  });
+
+  it('Testando a listagem de sales por id', async function () {
+    sinon.stub(connection, 'execute').resolves([idSalesDb]);
+    const response = await chai
+      .request(app)
+      .get('/sales/1')
+
+
+    expect(response.status).to.equal(200);
+    expect(response.body).to.deep.equal(idSalesDb);
+  });
   afterEach(sinon.restore);
-
-  it('Testando o registro de um produto ', async function () {
-    sinon.stub(connection, 'execute').resolves(true);
-
-    const response = await chai
-      .request(app)
-      .post('/sales')
-      .send(
-        [
-          {
-            "productId": 1,
-            "quantity": 1
-          },
-          {
-            "productId": 2,
-            "quantity": 5
-          }
-        ]
-      );
-
-    expect(response.status).to.equal(201);
-    expect(response.body).to.
-      deep.equal({
-        "id": 4,
-        "itemsSold": [
-          {
-            "productId": 1,
-            "quantity": 1
-          },
-          {
-            "productId": 2,
-            "quantity": 5
-          }
-        ]
-      });
-  });
-
-  it('Testando falha no cadastro sem productId ', async function () {
-    sinon.stub(connection, 'execute').resolves(true);
-
-    const response = await chai
-      .request(app)
-      .post('/sales')
-      .send([
-        {
-          "quantity": 1
-        },
-        {
-          "productId": 2,
-          "quantity": 5
-        }
-      ]
-      );
-
-    expect(response.status).to.equal(400);
-    expect(response.body).to.
-      deep.equal({
-        "message": "\"productId\" is required"
-      });
-  });
-
-  it('Testando falha no cadastro sem quantity ', async function () {
-    sinon.stub(connection, 'execute').resolves(true);
-
-    const response = await chai
-      .request(app)
-      .post('/sales')
-      .send([
-        {
-          "productId": 1
-        },
-        {
-          "productId": 2,
-          "quantity": 5
-        }
-      ]
-      );
-
-    expect(response.status).to.equal(400);
-    expect(response.body).to.
-      deep.equal({
-        "message": "\"quantity\" is required"
-      });
-  });
-
-  it('Testando falha no cadastro produto inexistente ', async function () {
-    sinon.stub(connection, 'execute').resolves(true);
-
-    const response = await chai
-      .request(app)
-      .post('/sales')
-      .send([
-        {
-          "productId": 10,
-          "quantity": 1
-        },
-        {
-          "productId": 2,
-          "quantity": 5
-        }
-      ]
-      );
-
-    expect(response.status).to.equal(400);
-    expect(response.body).to.
-      deep.equal({ "message": "Product not found" });
-  });
-
-  it('Testando falha no cadastro sem quantity maior que 0 ', async function () {
-    sinon.stub(connection, 'execute').resolves(true);
-
-    const response = await chai
-      .request(app)
-      .post('/sales')
-      .send([
-        {
-          "productId": 1,
-          "quantity": 0
-        },
-        {
-          "productId": 2,
-          "quantity": 5
-        }
-      ]);
-
-    expect(response.status).to.equal(400);
-    expect(response.body).to.
-      deep.equal({
-        "message": "\"quantity\" must be greater than or equal to 1"
-      });
-  });
 });
